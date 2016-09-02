e98050812c603b345da977422167f601370c7d25
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2016-09-02 18:10:57 -0400

    Cleaning up code

--- src/main/scala/io/basilic/myproject/Main.scala
+++ src/main/scala/io/basilic/myproject/Main.scala
@@ -6,8 +6,6 @@ import org.apache.spark.streaming.{StreamingContext, Seconds}
 object Main {
 
   def main(args: Array[String]) {
-  
-    /* Configuring Spark: https://spark.apache.org/docs/latest/configuration.html */
     val conf = new SparkConf().setAppName("SparkProjectTemplate")
     val ssc = new StreamingContext(conf, Seconds(1))
     val meetupStream = MeetupDStream(ssc)
