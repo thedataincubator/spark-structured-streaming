2a2de7bb2b74b00f1a3eb3471902b506ebcd0969
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2016-09-18 21:44:31 -0400

    Save and print modes

--- meetup/src/main/scala/com/thedataincubator/MeetupStreaming/Main.scala
+++ meetup/src/main/scala/com/thedataincubator/MeetupStreaming/Main.scala
@@ -1,5 +1,6 @@
 package com.thedataincubator.MeetupStreaming
 
+import org.apache.log4j.{Logger, Level}
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{StreamingContext, Seconds}
 
@@ -9,10 +10,16 @@ object Main {
     val conf = new SparkConf().setAppName("MeetupStreaming")
     val ssc = new StreamingContext(conf, Seconds(1))
     val meetupStream = MeetupDStream(ssc)
-    meetupStream.saveAsTextFiles("output/output", "part")
-
+    args match {
+      case Array() =>
+        // Disable logging to make messages more clear
+        Logger.getLogger("org").setLevel(Level.OFF)
+        Logger.getLogger("akka").setLevel(Level.OFF)
+        twitterDStream.print
+      case Array(output) => twitterDStream.saveAsTextFiles(s"output/$output", "txt")
+      case _ => throw new IllegalArgumentException("Expecting at most one argument");
+    }
     ssc.start()
     ssc.awaitTermination()
   }
-
 }
